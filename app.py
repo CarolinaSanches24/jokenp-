45fcf282b7ddc0c8e2e6763660c9ff0e07bdc015
CAROL SANCHES <135077050+CarolinaSanches24@users.noreply.github.com>
2024-02-05 18:25:25 +0000

    feat:adicionado logica jokenpo

--- app.py
+++ app.py
@@ -1,11 +1,50 @@
-#-----------------------------------------------------------------------------------------
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License. See LICENSE in the project root for license information.
-#-----------------------------------------------------------------------------------------
+import random
 
-from flask import Flask
-app = Flask(__name__)
+options = ["pedra","papel","tesoura"]
+score = 0;
+rounds_played = 0; #quantas vezes foi jogado
 
-@app.route("/")
-def hello():
-    return app.send_static_file("index.html")
+while True:
+    #inicializando o adversario
+    random_choice = random.choice(options);
+    player_choice = input("Escolha pedra, papel ou tesoura: ");
+
+    if(player_choice=="pedra"):
+        rounds_played +=1;
+        if random_choice=="pedra":
+            print("Empate")
+        elif random_choice =="papel":
+            print("Você perdeu!")
+        elif random_choice == "tesoura":
+            print("Você Ganhou!")
+            score +=1;
+
+    elif player_choice == "papel":
+        rounds_played +=1;
+        if random_choice=="papel":
+            print("Empate")
+        elif random_choice =="tesoura":
+            print("Você perdeu!")
+        elif random_choice == "pedra":
+            print("Você Ganhou!")
+            score +=1;
+
+    elif player_choice == "tesoura":
+        rounds_played +=1;
+        if random_choice=="tesoura":
+            print("Empate")
+        elif random_choice =="pedra":
+            print("Você perdeu!")
+        elif random_choice == "papel":
+            print("Você Ganhou!")
+            score +=1;
+
+    else:
+        play_again = input("Deseja jogar novamente? (s/n):")
+        if play_again == "s":
+            continue
+        elif play_again == "n":
+            print("Você ganhou :", score, " vezes!", rounds_played, " rodadas jogadas")
+            break
+        else:
+            print("Escolha inválida!")
